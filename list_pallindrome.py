c024ce3bc71a644db9e118d78044964ff1c6f7a2
unknown <abhilashasingh25>
2025-07-08 22:46:22 +0530

    commit

---
+++ list_pallindrome.py
@@ -0,0 +1,11 @@
+# write a program to check if a list contains a palindrome of elements 
+input_string = input("enter the elements of the list,seperated by spaces:")
+original_list = input_string.split()
+print(original_list)
+print(type(original_list))
+copy_list = original_list.copy()
+copy_list.reverse()
+if(copy_list == original_list):
+    print("pallindrome")
+else:
+    print("not pallindrome")  
