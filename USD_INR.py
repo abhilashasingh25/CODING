35e04cf2393184ae17545ea6d46ead05158c5a8f
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-14 20:03:14 +0530

    USD to INR

---
+++ USD_INR.py
@@ -0,0 +1,10 @@
+# write a function to convert USD to INR
+
+def converter (usd_val):
+    inr_val = usd_val*83
+    print(usd_val,"USD=",inr_val,"INR")
+
+converter(1)
+converter(2)
+converter(73)
+
