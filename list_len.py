ebade6765ad0e2952444c18c7d9436dcbb87ca18
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-14 20:09:34 +0530

    lenght of a list

---
+++ list_len.py
@@ -0,0 +1,9 @@
+# write a function to print the lenght og a list (list is the parameters)
+
+cities=["delhi","gurgaon","noida0","pune","mumbai","chennai"]
+heros=["thor","ironman","captain america","shaktimaam"]
+def print_len(list):
+    print(len(list))
+
+print_len(cities) 
+print_len(heros)   
