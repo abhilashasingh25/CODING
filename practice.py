c523ebff21574a1fda827b86cf9ff1a55281ae62
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-16 23:52:41 +0530

    input output

---
+++ practice.py
@@ -0,0 +1,11 @@
+# create a new file "practice.txt" using python. Add the following data in it
+""""
+Hi everyone
+we are lesrning File I/O
+using Java
+I like programming in Java
+"""
+
+with open("practice.txt","w")as f:
+    f.write("Hi everyone\n we are learning File I/O")
+    f.write("using Java\n I like programming in Java")    
