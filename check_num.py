def4203e6e75aefbeb52ad1e206b23ce90d03848
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-21 21:20:14 +0530

    check num +ve,-nr or 0 using if-elif-else

---
+++ check_num.py
@@ -0,0 +1,10 @@
+# python program to check if a number is positive ,negative or zero using if elif else
+
+num = float(input("enter a number:"))
+
+if num > 0:
+    print("positive number")
+elif num == 0:
+    print("zero")
+else:
+    print("negative number")        
