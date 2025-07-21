877b0728f7957f651db81f7c9609e79b2b593e61
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-21 23:23:33 +0530

    converting kilograms to miles

---
+++ kg_miles.py
@@ -0,0 +1,13 @@
+# python program to convert kilometers to miles
+
+kilometers = float(input("enter value in kilometers:"))
+
+#conversion factor
+
+conv_fac = 0.621371
+
+# calculate miles
+
+miles = kilometers*conv_fac
+
+print(kilometers," kilometers is equal to ",miles," miles")
