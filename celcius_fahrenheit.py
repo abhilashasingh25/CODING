8d577d671d02e08da924f581b989e84f9a661bd2
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-21 23:22:14 +0530

    converting celcius to fahrenheit

---
+++ celcius_fahrenheit.py
@@ -0,0 +1,9 @@
+# python program to convert celcius to fahrenheit
+
+celcius = float(input("enter value in celcius:"))
+
+# calculate fahrenheit
+
+fahrenheit = (celcius*1.8) + 32
+
+print(celcius," celcius is equal to ",fahrenheit,"fahrenheit")
