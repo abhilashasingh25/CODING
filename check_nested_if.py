0afb87522639e4d84e477a9dfab07d6a142ff7f0
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-21 21:15:18 +0530

    check num +ve, -ve or 0 using nested if

---
+++ check_nested_if.py
@@ -0,0 +1,11 @@
+# python program to check if a number is positive , negative or zero using nested if
+
+n = float(input("enter a number:"))
+
+if n >=0 :
+    if n ==0:
+        print("zero")
+    else:
+        print("positive number")
+else:
+    print("negative number")            
