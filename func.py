24f45d1edc773861368a03f634330f9b7ce6e961
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-21 23:32:59 +0530

    add 10 to a given number

---
+++ func.py
@@ -0,0 +1,15 @@
+# write a function to add 10 to a given number
+
+# get the number from the user
+
+n = int (input("enter the number:"))
+
+def func(n):
+
+    s= n+10
+    print(s)
+    return
+
+func(n)
+
+
