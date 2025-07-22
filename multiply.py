3b5df9000021c7ca75650c9523b11d08804e1f89
abhilasha <abhilashasingh2k15@gmail.com>
2025-07-22 22:50:30 +0530

    multipication of a number

---
+++ multiply.py
@@ -0,0 +1,11 @@
+# python program to display the multiplication table
+
+# take input from user
+
+n= int(input("enter the number:"))
+
+# iterate 10 times from i =1 to 10
+
+for i in range (1,11):
+    print(n ,"*" ,i," =",n*i)
+    
