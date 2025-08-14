755777e38c093906b70ddf17169b63218a09f28c
abhilasha <abhilashasingh2k15@gmail.com>
2025-08-14 23:30:05 +0530

    illustrate different set operations

---
+++ set_operation.py
@@ -0,0 +1,16 @@
+# python program to illustrate different set operations
+
+# define three sets
+
+E ={0,2,4,6,8};
+N ={1,2,3,4,5};
+
+# SET union
+print("union of E and N is:",E|N)
+
+# SET INTERACTION
+print("interaction of E and N is:",E-N)
+
+# SET SYMMETRIC DIFFERENCES
+
+print("symmetric differnces of E and N is",E^N)
