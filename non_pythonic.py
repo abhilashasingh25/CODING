43790aa99fb6679191b0c8706564b60f0f523dcd
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-18 22:33:58 +0530

    flatten a nest list using nested for loop

---
+++ non_pythonic.py
@@ -0,0 +1,11 @@
+# python program to flatten a nest list using nested for loop(non pythonic way)
+
+my_list =[[1],[2,3],[4,5,6,7]]
+
+flat_list=[]
+
+for sublist in my_list:
+    for num in sublist:
+        flat_list.append(num)
+
+print(flat_list)        
