5f1d9f7aad6cb9e07b56bff1e6d3e6e906c4fa87
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-18 22:32:27 +0530

    flatten a nested list using itertool package

---
+++ itertool.py
@@ -0,0 +1,9 @@
+# python program to flatten a nested list using itertool package
+
+import itertools
+
+my_list =[[1],[2,3],[4,5,6,7]]
+
+flat_list=list(itertools.chain(*my_list))
+
+print(flat_list)
