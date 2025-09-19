a888c8bc42d8d93a4d101a842eae98fff2ef732e
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-19 22:31:26 +0530

    acces both key and value using items

---
+++ iterate_dictionaries.py
@@ -0,0 +1,6 @@
+# python program to iterate over dictionaries using for loop
+# access both ket and value using items()
+
+dt={'a':'juice','b':'grill','c':'corn'}
+for key,value in dt.items():
+    print(key,value)
