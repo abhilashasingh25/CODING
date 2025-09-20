2a77cae043c11bc8c9f389384d7005c8134acec7
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-20 23:34:40 +0530

    parse a string to a float or int

---
+++ parse.py
@@ -0,0 +1,14 @@
+# python program to parse a string to a float or int
+
+# parse string into integer
+
+a_str ="1500"
+
+a_int= int(a_str)
+
+# print the type
+
+print(type(a_int))
+
+# print the value
+print(a_int)
