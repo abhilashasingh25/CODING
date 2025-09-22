6694e68196bd9bcb72722e50788ff7b85406d322
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-22 23:29:29 +0530

    get a substing of a string

---
+++ substring.py
@@ -0,0 +1,17 @@
+# python program to get a substring of a string
+
+# using string slicing
+
+my_string ="i love python."
+
+# print"love"
+
+print(my_string[2:1])
+
+# print "love python"
+
+print(my_string[2:])
+
+# print "i love python"
+
+print(my_string[:-1])
