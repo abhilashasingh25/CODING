a44f61d1cc87f099626bac6d2f4be3167822a271
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-22 23:28:30 +0530

    output without a newline using end keyword

---
+++ newline.py
@@ -0,0 +1,16 @@
+# python program to print output without a newline using end keyword
+
+# print each statment on a new line
+
+print("python")
+print("is easy to learn.")
+
+# new line
+
+print()
+
+# print both the statment of a single line
+
+print("python",end=" ")
+
+print("is easy to learn")
