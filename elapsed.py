18cdd6b91dc885767653172833e2df9718ab51f6
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-23 18:15:41 +0530

    measure the elapsed time in python

---
+++ elapsed.py
@@ -0,0 +1,16 @@
+# python program to measure the elapsed time in python
+
+# using time module
+
+import time
+
+# start timestamp
+
+start = time.time()
+
+print(23*23)
+
+# save timestamp
+
+end =time.time()
+print(end-start)
