68c29385ed4e18a7ba166115d15a1d5409005123
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-23 18:12:16 +0530

    convert two dictionary using zip and dict methods

---
+++ convert.py
@@ -0,0 +1,11 @@
+# python program to convert two lists into a dictionary
+
+# using zip and dict methods
+
+index =[1,2,3]
+
+language =['python','c','c++']
+
+dictionary =dict(zip(index,language))
+
+print(dictionary)
