d2194822a21d76b182c3388a8b28825b52618ee3
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-24 21:43:49 +0530

    use parenthesis and a single/double quotes

---
+++ multiline.py
@@ -0,0 +1,23 @@
+# python program to create a lomg multiline string
+
+# use triple quotes
+
+str =""" this is the only way to learn to program which is by writing code."""
+
+print(str)
+
+# use parentheses and a single / double quotes
+
+str = ('the only way to \n'
+'learn to program \n'
+'is by writng the code')
+
+print(str)
+
+# using \
+
+str ="the only way\n"\
+"learn to program is\n"\
+"by writing code."
+
+print(str)
