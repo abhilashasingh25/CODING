1c39de473e440e0943b494338956209d2e5fde19
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-24 21:40:04 +0530

    return multiple values from a function using a dictionary

---
+++ dictionay.py
@@ -0,0 +1,13 @@
+# python program to return multiple values from a fuction 
+
+# using a dictionary
+
+def name():
+    n1= "john"
+    n2 ="anshu"
+
+    return {1:n1,2:n2}
+
+name= name()
+
+print(name)
