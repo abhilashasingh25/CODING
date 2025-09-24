cab2e663d702b256219dc5bd32b81900b836f7bf
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-24 21:41:10 +0530

    return multiple values from a function

---
+++ multiple.py
@@ -0,0 +1,16 @@
+# python program to return multiple values from a function
+
+# return values using comma
+
+def name():
+    return "John","Anshu"
+
+# print the tuple with the returned value
+
+print(name())
+
+# get the individual items
+
+name_1, name_2 =name()
+
+print(name_1,name_2)
