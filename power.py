0b885c538fa47e12ccfd1cb9bfa56d5ccafb2495
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-25 18:36:04 +0530

    calculate the power of a number using a while loop

---
+++ power.py
@@ -0,0 +1,11 @@
+# python program to comput the power of a number
+
+# calculate power of a number using a while loop
+
+base =3
+exponent =4
+result =1
+while exponent !=0:
+    result *=base
+    exponent -=1
+    print("answer="+str(result))
