864d6477f378fe63c577a62b3bb5167abe762b2f
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-25 18:34:11 +0530

    calculate the power of a number using pow() function

---
+++ pow.py
@@ -0,0 +1,9 @@
+# python program to calculate the power of a number
+
+# calculate the power of a number using pow() function
+
+base =3 
+exponent = -4
+result =pow(base,exponent)
+
+print("answer=" +str(result))
