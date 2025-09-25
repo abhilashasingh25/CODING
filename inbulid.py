d0a9dae637e7c80e3a42d96c437ddf62c9bddc7b
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-25 18:29:10 +0530

    capitalize the first character of a string using inbuilt method calitalize()

---
+++ inbulid.py
@@ -0,0 +1,9 @@
+# python program to capitalize the first character of a string
+
+# using inbuilt method capitalize()
+
+my_string ="programiz is lit"
+
+cap_string = my_string.capitalize()
+
+print(cap_string)
