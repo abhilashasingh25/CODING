68421130e62b751b2eb68e9bd4d5ab4eff1b8f82
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-26 23:43:26 +0530

    compute all the permutation of a string

---
+++ import.py
@@ -0,0 +1,8 @@
+# python program to compute all the permutation of a string 
+
+# using itertools
+
+from itertools import permutations
+
+word =[' '.join(p)for p in permutations("pro")]
+print(word)
