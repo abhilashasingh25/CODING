725e2a63fc17ea5afa2102ba8d034a3a691b7b29
abhilasha <abhilashasingh2k15@gmail.com>
2025-09-26 23:41:43 +0530

    count the number of occurance of character in string

---
+++ character.py
@@ -0,0 +1,11 @@
+# python program to count the number of occurance of a character in string
+
+# using for loop
+
+count=0
+my_string="programiz"
+my_char ='r'
+for i in my_string:
+    if i == my_char:
+        count +=1
+print(count)
